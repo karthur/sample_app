8380dd5596cd2fd89ab0cf028afa461b3a0e6602
karthur <karthur@yeah.net>
2010-08-31 19:16:03 +0800

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "about"
   end
 
+  def contact
+    @title = "Contact"
+  end
+
+  def help
+    @title ="Help"
+  end
 end
